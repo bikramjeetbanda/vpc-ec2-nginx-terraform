8b69a9d2e04098f313668b1ec7765382eef9b86b
bjeet-vrify <bikramjeet@vrify.com>
2025-08-27 12:08:00 -0700

    Created 3 private subnets in 3 different AZs with route tables but no internet connectivity

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -28,3 +28,20 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.azs[count.index]
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
