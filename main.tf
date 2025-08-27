fe40d61eb16ab26721e53aade0387445587f3318
bjeet-vrify <bikramjeet@vrify.com>
2025-08-27 13:55:30 -0700

    Created security group for ec2 instance with 80 port allowed for nginx

--- main.tf
+++ main.tf
@@ -9,3 +9,23 @@ module "vpc" {
   private_subnet_cidrs = var.private_subnet_cidrs
   azs                  = var.azs
 }
+
+resource "aws_security_group" "nginx_sg" {
+  name        = "nginx-sg"
+  description = "Allow HTTP traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
