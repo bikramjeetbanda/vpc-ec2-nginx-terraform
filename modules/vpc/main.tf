3a50966df4a514812d60d92eb78c6cbaf226457d
bjeet-vrify <bikramjeet@vrify.com>
2025-08-27 15:04:06 -0700

    Added NAT gateway to enable internet access for private subnets indirectly

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -36,8 +36,23 @@ resource "aws_subnet" "private" {
   availability_zone = var.azs[count.index]
 }
 
+resource "aws_eip" "nat" {
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+  depends_on    = [aws_internet_gateway.igw]
+}
+
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
 }
 
 resource "aws_route_table_association" "private" {
