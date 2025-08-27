882c986209a23f2e16696fcc49183df9b29c4756
bjeet-vrify <bikramjeet@vrify.com>
2025-08-27 11:45:38 -0700

    Added 3 public subnets across 3 different AZs with route table and Internet gateway routing for intenet access

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,3 +5,26 @@ resource "aws_vpc" "main" {
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  availability_zone = var.azs[count.index]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,3 +1,11 @@
 variable "vpc_cidr" {
   type = string
 }
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "azs" {
+  type = list(string)
+}
